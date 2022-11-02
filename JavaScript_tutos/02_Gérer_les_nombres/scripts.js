dd97de543bd2e3472828a217124c87bb865882a9
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-02 10:01:52 +0100

    JavaScript Gérer les nombres

---
+++ JavaScript_tutos/02_Gérer_les_nombres/scripts.js
@@ -0,0 +1,43 @@
+let nombre1 = 12;
+let nombre2 = 25;
+
+// Opérations de base +, -, *, /
+let nombre = nombre1 + nombre2;
+
+nombre = nombre1 - nombre2;
+
+nombre = nombre1 * nombre2;
+
+nombre = nombre1 / nombre2;
+
+// Priorité des opérations la multiplication et la division sont prioritaires sur l'addition et la soustraction. Mettre les parenthèses.
+nombre = (2 + 3) * 5;
+
+// Modulo, le reste d'une division
+
+nombre = nombre1 % nombre2;
+
+nombre = 15 % 4;
+
+// incrémenter de 4
+nombre = nombre + 4;
+nombre +=4;
+nombre +=1;
+
+// Incrément de 1 raccourci
+nombre++;
+
+// Décrémenter de 3
+nombre = nombre - 3;
+nombre-=3;
+
+// Décrémenter de 1 raccourci
+nombre--;
+
+
+// Raccourci division par 4
+nombre /= 4;
+
+
+// Ecrire dans la console
+console.log(nombre);
