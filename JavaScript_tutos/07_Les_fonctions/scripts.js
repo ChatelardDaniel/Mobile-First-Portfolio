4bc9cc08376b2f34ed612a7a7a91c8eb0a005bd2
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-02 10:14:54 +0100

    Création du sommaire des cours JavaScript

---
+++ JavaScript_tutos/07_Les_fonctions/scripts.js
@@ -0,0 +1,21 @@
+// Intéraction avec l'utilisateur
+// alert("message");
+// prompt("Quel nombre ?");
+// confirm("Etes vous sûr ?");
+
+/********** Les variables **********/
+// On crée la variable nombre et on lui met la valeur 3
+let nombre = 3;
+
+var autreNombre = 2;
+
+nombre = 25;
+
+let texte ="Bonjour";
+
+autreNombre = 15.36;
+
+// Ecrire dans la console
+console.log("Brouette");
+console.log(nombre);
+console.log(texte);
