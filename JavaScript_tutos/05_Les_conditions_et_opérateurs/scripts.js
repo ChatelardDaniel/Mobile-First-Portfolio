31676d8e698243a291eeb3e15e4f07f1f2afae03
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-02 14:25:06 +0100

    Ajout du cours Les conditions et opérateurs de comparaison

--- JavaScript_tutos/05_Les_conditions_et_opérateurs/scripts.js
+++ JavaScript_tutos/05_Les_conditions_et_opérateurs/scripts.js
@@ -1,21 +1,54 @@
-// Intéraction avec l'utilisateur
-// alert("message");
-// prompt("Quel nombre ?");
-// confirm("Etes vous sûr ?");
+// Conditions et opérations de comparaison.
 
-/********** Les variables **********/
-// On crée la variable nombre et on lui met la valeur 3
-let nombre = 3;
+let nombre = 12;
 
-var autreNombre = 2;
+// == est une égalité faible, compare que la valeur, pas le type.
+// === égalité stricte, compare la valeur et le type.
+if(nombre === 12){
+    console.log("Le nombre est égal à 12");
+}else{
+    console.log("Le nombre n'est pas égal à 12");
+}
 
-nombre = 25;
+/* != inégalité faible */
+/* !== inégalité stricte */
+nombre = 13;
+if(nombre != 12){
+    console.log("Le nombre n'est pas égal à 12");
+}else{
+    console.log("Le nombre est égal à 12");
+}
 
-let texte ="Bonjour";
+// < inférieur à
+// <= inférieur ou égal
+// > supérieur
+// >= supérieur ou égal
+if(nombre < 12){
+    console.log("Le nombre est plus petit que 12");
+}else{
+    console.log("Le nombre n'est pas plus petit que 12");
+}
 
-autreNombre = 15.36;
+// vérifier un booléen
+let bool = true;
+if(bool){
+    console.log("Le booléen est à true")
+}else{
+    console.log("Le booléen est à false")
+}
 
-// Ecrire dans la console
-console.log("Brouette");
-console.log(nombre);
-console.log(texte);
+// Vérifier un booléen et nombre
+// && AND
+if(bool && nombre < 12){
+    console.log("Le booléen est à true et le nombre est plus petit que 12")
+}else{
+    console.log("Le booléen est à false")
+}
+
+// Vérifier un booléen ou nombre
+// || OU
+if(bool || nombre < 12){
+    console.log("Le booléen est à true ou le nombre est plus petit que 12")
+}else{
+    console.log("Le booléen est à false")
+}
