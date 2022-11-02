40bae2ed1f45c84a7bf090ac1c60ad9608bde5a3
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-02 11:07:04 +0100

    Ajout du cours gérer les textes

--- JavaScript_tutos/03_Gérer_les_textes/scripts.js
+++ JavaScript_tutos/03_Gérer_les_textes/scripts.js
@@ -1,21 +1,37 @@
-// Intéraction avec l'utilisateur
-// alert("message");
-// prompt("Quel nombre ?");
-// confirm("Etes vous sûr ?");
+// Manipuler les chaînes de caractères
+let texte = "J'ai une jolie brouette";
 
-/********** Les variables **********/
-// On crée la variable nombre et on lui met la valeur 3
-let nombre = 3;
+// l'antislash, permet déchapper un caratère '\'
+let texte2 = 'J\'ai un nouveau texte';
 
-var autreNombre = 2;
+// le backtick 
+let texte3 = `Un autre texte`;
 
-nombre = 25;
+// La concaténation
+let prenom = "Benoit";
+let nom = "Dupont";
+let phrase = "Bonjour " + prenom +" " + nom;
 
-let texte ="Bonjour";
+// Concaténation avec les Backticks
+let phrase2 =`Bonjour ${prenom} ${nom}`;
 
-autreNombre = 15.36;
+// donne moi le quatrième caractère
+//console.log(phrase2[3]);
 
-// Ecrire dans la console
-console.log("Brouette");
-console.log(nombre);
-console.log(texte);
+// Longueur de la chaîne de caractère avec 'length'
+console.log(phrase2.length);
+
+// savoir si une chaine contient le mot Benoit, renverra -1 s'il ne la pas trouve, ou une nombre qui correspondra à la position de la première lettre du mot.
+console.log(phrase2.indexOf("Benoit"));
+
+// choisir une partie de la chaîne de caractères à afficher. Exemple entre 8 et 12.
+console.log(phrase2.slice(8, 12));
+
+// Mettre en Majuscule
+console.log(phrase2.toUpperCase());
+
+// Mettre en Minuscule
+console.log(phrase2.toLowerCase());
+
+// Remplace un mot par un autre avec replace()
+console.log(phrase2.replace("Benoit", "Bruno"));
