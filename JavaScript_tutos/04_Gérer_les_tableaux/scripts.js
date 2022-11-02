0ddca2d196d6b36e16d388a0066e51aa1ff24cc0
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-02 13:56:04 +0100

    Ajout du cours gérer les tableaux

--- JavaScript_tutos/04_Gérer_les_tableaux/scripts.js
+++ JavaScript_tutos/04_Gérer_les_tableaux/scripts.js
@@ -1,21 +1,41 @@
-// Intéraction avec l'utilisateur
-// alert("message");
-// prompt("Quel nombre ?");
-// confirm("Etes vous sûr ?");
+// Manipuler les tableaux
+let tableau = [10, "Brouette",true, 18.25];
 
-/********** Les variables **********/
-// On crée la variable nombre et on lui met la valeur 3
-let nombre = 3;
+console.log(tableau);
 
-var autreNombre = 2;
+// console.table(), permet d'afficher un mini tableau dans la console.
+console.table(tableau);
 
-nombre = 25;
+// afficher le mot Brouette du tableau, tapez son index.
+console.log(tableau[1]);
 
-let texte ="Bonjour";
+// inverser un tableau avec reverse()
+tableau.reverse();
+console.table(tableau);
 
-autreNombre = 15.36;
+// Ajouter une ou plusieurs valeur dans un tableau
+// au début
+tableau.unshift("Bonjour", "Benoit");
+console.table(tableau);
 
-// Ecrire dans la console
-console.log("Brouette");
-console.log(nombre);
-console.log(texte);
+// à la fin
+tableau.push("Au revoir", 14);
+console.table(tableau);
+
+// Retirer une valeur du tableau
+// La 1er
+let valeur = tableau.shift();
+console.table(valeur);
+
+// La dernière
+valeur = tableau.pop();
+console.table(valeur);
+
+// Modifier des valeurs dans le tableau
+tableau.splice(2, 0, "Inséré");
+console.table(tableau);
+
+// Modifier des valeurs dans le tableau
+// Ajout à l'index 2 et supprimer les trois suivants
+tableau.splice(2, 3, "Inséré");
+console.table(tableau);
