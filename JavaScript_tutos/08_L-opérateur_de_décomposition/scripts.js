fbd6ff54119a60e2962092b01a1c10dc86107dda
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-03 18:09:04 +0100

    Ajout du cours L'opérateur de décomposition(spread Operator)

--- JavaScript_tutos/08_L-opérateur_de_décomposition/scripts.js
+++ JavaScript_tutos/08_L-opérateur_de_décomposition/scripts.js
@@ -1,21 +1,57 @@
-// Intéraction avec l'utilisateur
-// alert("message");
-// prompt("Quel nombre ?");
-// confirm("Etes vous sûr ?");
+// L'opérateur de décomposition "spread operator"
 
-/********** Les variables **********/
-// On crée la variable nombre et on lui met la valeur 3
-let nombre = 3;
+const nombre = [25, 12, 8];
 
-var autreNombre = 2;
+function addition(nb1, nb2, nb3){
+    return nb1 + nb2 + nb3;
+}
 
-nombre = 25;
+let resultat = addition(10, 20, 30);
 
-let texte ="Bonjour";
+console.log(resultat);
 
-autreNombre = 15.36;
+// Avec le "spread operator"
 
-// Ecrire dans la console
-console.log("Brouette");
-console.log(nombre);
-console.log(texte);
+const nombre2 = [25, 12, 8];
+
+function addition2(nb1, nb2, nb3){
+    return nb1 + nb2 + nb3;
+}
+
+let resultat1 = addition2(...nombre2);
+
+console.log(resultat1);
+
+// Avec l'objet date
+
+const tableauDate = [2022, 1, 15];
+let maDate = new Date(...tableauDate);
+
+console.log(maDate)
+
+// inclure un tableau dans un autre tableau avec le spread operator
+
+const tableauNombre = [48, 74, ...nombre, 26];
+
+console.table(tableauNombre);
+
+// concaténer deux tableaux avec le spread operator
+
+const t1 = ["a", "b", "c"];
+const t2 = ["d", "e", "f"];
+
+const t3 = [...t1, ...t2];
+console.table(t3);
+
+// Avec des objets avec le spread operator
+
+const nom = {
+    nom: "Dupont"
+};
+
+const prenom = {
+    prenom: "Bruno"
+};
+
+const profil = {...nom, ...prenom};
+console.log(profil);
