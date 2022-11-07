8a4c1983975cee8d94eeb50642a4d0e064dc40b3
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-07 15:27:50 +0100

    Ajout du cours Capturer et enregistrer une signature avec canvas

---
+++ JavaScript_tutos/21_Capturer_et_enregistrer_une_signature_avec_canvas/readme.md
@@ -0,0 +1,3 @@
+# Lancer php via le terminal
+
+Tapez: php -S localhost:8000 puis enter
---
+++ JavaScript_tutos/21_Capturer_et_enregistrer_une_signature_avec_canvas/signature.php
@@ -0,0 +1,17 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=== 'POST'){
+    $data = json_decode(file_get_contents('php://input'), false);
+    
+    list($type, $data) = explode(';', $donnees->image);
+    list(, $image) = explode(',', $data);
+
+    $image_decodee = base64_decode($image);
+
+    $fichier = md5(uniqid()). '.png';
+
+    if(file_put_contents(__DIR__."/images/$fichier", $image_decodee)){
+        echo 'Succcès';
+    }else{
+        echo 'Echec';
+    }
+}
