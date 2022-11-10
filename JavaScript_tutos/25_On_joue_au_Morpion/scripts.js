ec0f642dd959b549adc23a860d03138c7dcd4788
ChatelardDaniel <cdanasc@hotmail.fr>
2022-11-10 16:00:03 +0100

    Ajout du tutos Mopion

---
+++ JavaScript_tutos/25_On_joue_au_Morpion/scripts.js
@@ -0,0 +1,82 @@
+// On charge les informations utiles
+const statut = document.querySelector("h2")
+let jeuActif = true
+let joueurActif = "X"
+let etatJeu = ["","","","","","","","",""]
+
+
+const conditionsVictoire = [
+    [0, 1, 2],// lignes
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],// colonnes
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],// diagonales
+    [2, 4, 6]
+]
+
+// Messages
+const gagne = () => `Le joueur ${joueurActif} a gagné` 
+const egalite = () => "Egalité"
+const tourJoueur = () => `C'est au tour du joueur ${joueurActif}`
+
+statut.innerHTML = tourJoueur()
+
+// sélection chaqu'une des cases
+document.querySelectorAll(".case").forEach(cell => cell.addEventListener("click", gestionClickCase))
+// sur le bouton
+document.querySelector("#recommencer").addEventListener("click", recommencer)
+
+// fonction gestionClickCase
+function gestionClickCase(){
+    // On récupère l'index de la case cliquée
+    const indexCase = parseInt(this.dataset.index)
+    
+    if(etatJeu[indexCase] !== "" || !jeuActif){
+        return  // Je fais rien
+    }
+
+    etatJeu[indexCase] = joueurActif
+    this.innerHTML = joueurActif
+
+    verifGagne()
+}
+
+function verifGagne(){
+    let tourGagnant = false
+
+    for(let conditionVictoire of conditionsVictoire){
+        let val1 = etatJeu[conditionVictoire[0]]
+        let val2 = etatJeu[conditionVictoire[1]]
+        let val3 = etatJeu[conditionVictoire[2]]
+        if(val1 === "" || val2 === "" || val3 === ""){
+            continue
+        }
+        if(val1 === val2 && val2 === val3){
+            tourGagnant = true
+            break
+        }
+    }
+    if(tourGagnant){
+        statut.innerHTML = gagne()
+        jeuActif = false
+        return
+    }
+    if(!etatJeu.includes("")){
+        statut.innerHTML = egalite()
+        jeuActif = false
+        return
+    }
+
+    joueurActif = joueurActif === "X" ? "O" : "X"
+    statut.innerHTML = tourJoueur()
+}
+
+function recommencer(){
+    joueurActif = "X"
+    jeuActif = true 
+    let etatJeu = ["","","","","","","","",""]
+    statut.innerHTML = tourJoueur()
+    document.querySelectorAll(".case").forEach(cell => cell.innerHTML = "")
+}
